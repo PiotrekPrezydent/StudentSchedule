69b3ab5545daf48ec6ecb7cf089df2fd92947baf
Piotrek Prezydent <94804932+PiotrekPrezydent@users.noreply.github.com>
2025-05-28 22:43:54 +0200

    comment

--- StudentScheduleBackend/AppDbContextFactory.cs
+++ StudentScheduleBackend/AppDbContextFactory.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Design;
+﻿using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
 using StudentScheduleBackend;
 
+//mirgration commad:
+//dotnet ef migrations add InitialCreate --project StudentScheduleBackend --startup-project StudentScheduleClient
 internal class AppDbContextFactory : IDesignTimeDbContextFactory<Context>
 {
     public Context CreateDbContext(string[] args)
